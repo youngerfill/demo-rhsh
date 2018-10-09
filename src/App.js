456a4524bc9b5903f32f44b38e1c20487a3c296c
bergoid <bergoid@users.noreply.github.com>
2018-10-09 15:01:22 +0200

    A minimal working example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import * as h from 'react-hyperscript-helpers';
+//import { span } from 'react-hyperscript-helpers';
+
+const App = ({name}) =>
+{
+    return h.span("Hello " + name + "!")
+//    return span("Hello " + name + "!")
+//    return span(`Hello ${name}!`)
 }
 
 export default App;
